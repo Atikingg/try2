e971b38096a90961d61ff9bfcb74ee6422d771bd
Atikingg <atikaoutlook@gmail.com>
2019-02-21 21:41:24 +0300

    /

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ var tileSize = 10
 var rowTile = 5
 var columnTile = 5
 var tilesAmount = 2
-var tile = 0
 
 function draw() {
   for (var r = 0; r < tilesAmount; r++) {
     for (var c = 0; c < columnTile; c++) {
-      tile = ground[r][c]
+      var tile = ground[r][c]
+      var tileRow = (tile / tilesAmount) | 0
+      var tileCow = (tile % tilesAmount) | 0
+      context.drawImage(tilesAmount, (tileCow * tileSize), (tileRow * tileSize), tileSize, c * tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize)
     }
   }
 }
-
-var tileRow = (tile / tilesAmount) | 0
-var tileCow = (tile % tilesAmount) | 0
