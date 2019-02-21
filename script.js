fbb7534a43d978afb44a486ce7d7748f908eab89
Atikingg <atikaoutlook@gmail.com>
2019-02-21 21:26:13 +0300

    /

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var context = canvas.getContext('2d')
 
 var tiles = new Image();
 tiles.src = "img/tiles.png"
+var tileSize = 10
+var rowTile = 5
+var columnTile = 5
+var tilesAmount = 2
+
+function draw() {
+  for (var r = 0; r < tilesAmount; r++) {
+    for (var c = 0; c < columnTile; c++) {
+      var tile = ground[r][c]
+    }
+
+  }
+}
