2c55b66bc1eb905a48bd5ad07ad4b311c0023aa1
Atikingg <47576479+Atikingg@users.noreply.github.com>
2019-02-22 13:40:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ var context = canvas.getContext('2d')
 var tiles = new Image();
 tiles.src = "img/tiles.png"
 var tileSize = 10
-var rowTile = 5
-var columnTile = 5
+var rowTile = 20
+var columnTile = 20
 var tilesAmount = 2
+
+var ground = [
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
 
 function draw() {
   for (var r = 0; r < tilesAmount; r++) {
@@ -14,7 +20,9 @@ function draw() {
       var tile = ground[r][c]
       var tileRow = (tile / tilesAmount) | 0
       var tileCow = (tile % tilesAmount) | 0
-      context.drawImage(tilesAmount, (tileCow * tileSize), (tileRow * tileSize), tileSize, c * tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize)
+      context.drawImage(tiles, (tileCow * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize)
     }
   }
 }
+
+setInterval(draw, 10)
