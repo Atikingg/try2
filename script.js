c7b1bd225c0a1c2bb4ae79fc9c625e414719bf77
Atikingg <atikaoutlook@gmail.com>
2019-02-22 20:56:09 +0300

    /

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ var context = canvas.getContext('2d')
 
 var tiles = new Image();
 tiles.src = "img/tiles.png"
-var tileSize = 10
-var rowTile = 20
-var columnTile = 20
-var tilesAmount = 2
+var tileSize = 40
+var tileLength = 20
+var tileHeight = 20
+var tilesAmount = 5
 
-var ground = [
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+var locate = [
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
 ]
 
 function draw() {
-  for (var r = 0; r < tilesAmount; r++) {
-    for (var c = 0; c < columnTile; c++) {
-      var tile = ground[r][c]
-      var tileRow = (tile / tilesAmount) | 0
-      var tileCol = (tile % tilesAmount) | 0
-      context.drawImage(tiles, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize)
+  for (var i = 0; i < tileLength; i++) {
+    for (var j = 0; j < tileHeight; j++) {
+      var tileName = locate[i][j]
+      var tileRow = (tileName / tilesAmount) | 0
+      var tileCol = (tileName % tilesAmount) | 0
+      context.drawImage(tiles, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (j * tileSize),(i * tileSize), tileSize, tileSize)
     }
   }
 }
 
-setInterval(draw, 10)
+setInterval(draw,10)
